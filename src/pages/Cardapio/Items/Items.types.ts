f713bc1c312f13c8afe88eb53a091d38165d7f5f
Igor Josiel <igorjosielvitalino@gmail.com>
2023-04-06 10:46:50 -0300

    feat: arquivos de estilos e tipos do componente Items.

---
+++ src/pages/Cardapio/Items/Items.types.ts
@@ -0,0 +1,9 @@
+import { IOrdinationOptions } from "../Ordination/Ordination.types";
+
+export type IOrdinationType = 'size' | 'serving' | 'price';
+
+export interface IItemsProps {
+  searchEngine: string;
+  filter: number | null;
+  ordination: IOrdinationOptions;
+}
