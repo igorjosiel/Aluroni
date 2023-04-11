febbef02f3feaa80e8c90e7cb52789d86f01ecaf
Igor Josiel <igorjosielvitalino@gmail.com>
2023-04-11 09:24:37 -0300

    feat: ajustando o componente do roteamento do projeto.

--- src/router.js
+++ src/router.js
@@ -3,19 +3,24 @@ import Cardapio from 'pages/Cardapio';
 import Start from 'pages/Start';
 import Menu from 'components/Menu';
 import DefaultPage from 'components/DefaultPage';
+import About from 'pages/About';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
-      
         <Routes>
           <Route path='/' element={<DefaultPage />}>
             <Route index element={<Start />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<About />} />
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
